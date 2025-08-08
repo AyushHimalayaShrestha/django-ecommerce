63d9c50c885f3dddd08691927489e3154297ff0f
Ayush  Shrestha <aayush625@gmail.com>
2025-08-08 14:00:12 +0545

    product detail page

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category_name= models.CharField(max_length=100)
+    created_at= models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+     return self.category_name
+
+class Product(models.Model):
+    product_name= models.CharField(max_length=100, null=True)
+    product_price=models.IntegerField(null=True)
+    description=models.TextField(null=True)
+    quantity=models.IntegerField(null=True)
+    image= models.URLField(null=True)
+    category= models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+     return self.product_name
+
--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from . models import Product
 # Create your views here.
 
+
+
 def products(request):
-    return HttpResponse("Product Lists")
+    product_lists =Product.objects.all().order_by('-id')
+    item={
+        "products":product_lists
+    }
+    return  render(request, 'product/productlists.html',item)
+
+def product_details(request,product_id):
+    product = Product.objects.get(id =product_id)
+
+    data={
+       'product':product
+
+    }
+    return render(request,'product/product_details.html',data)
