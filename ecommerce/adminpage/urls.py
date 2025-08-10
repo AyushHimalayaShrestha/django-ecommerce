43bc9c352bcc14dab0d98d4b075f400d874a5cc3
Ayush  Shrestha <aayush625@gmail.com>
2025-08-10 20:59:52 +0545

    admin add_product

--- ecommerce/adminpage/urls.py
+++ ecommerce/adminpage/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns =[
 
     path('category/add/', views.add_category, name='add_category'),
+    path('product/add/',views.add_product, name='add_product')
    
     
 ]
--- ecommerce/adminpage/views.py
+++ ecommerce/adminpage/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
-from  product.forms import CategoryForm
+from  product.forms import *
+
 # Create your views here.
 
 def add_category(request):
     if request.method == 'POST':
-        form = CategoryForm()
+        form = CategoryForm(request.POST)
         if form.is_valid():
             form.save()
             redirect('add_category')
@@ -13,3 +14,15 @@ def add_category(request):
         form = CategoryForm()        
 
     return render(request,'add_category.html',{'form':form})
+
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            redirect('add_product')
+
+    else:
+        form=ProductForm()
+    return render(request,'add_product.html',{'form':form})
