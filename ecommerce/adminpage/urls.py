5165bebcc8ae9a8cbe858adaef354fe9c15cb835
Ayush  Shrestha <aayush625@gmail.com>
2025-08-12 13:52:12 +0545

    update product

--- ecommerce/adminpage/urls.py
+++ ecommerce/adminpage/urls.py
@@ -4,7 +4,10 @@ urlpatterns =[
 
     path('category/add/', views.add_category, name='add_category'),
     path('product/add/',views.add_product, name='add_product'),
-    path('product/lists',views.products, name="dashboard_product_lists")
+    path('product/lists',views.products, name="dashboard_product_lists"),
+    path('product/update/<int:product_id>/',views.update_product,name='update_product'),
+    path('product/delete/<int:product_id>/',views.delete_product,name='delete_product'),
+    path('category/list/',views.category,name='dashboard_category_lists')
    
     
 ]
--- ecommerce/adminpage/views.py
+++ ecommerce/adminpage/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from  product.forms import *
 from product.models import *
-
+from django.contrib import messages
 # Create your views here.
 
 def add_category(request):
@@ -34,3 +34,46 @@ def add_product(request):
 def products(request):
     product_lists = Product.objects.all()
     return render(request,'dashboard_product_lists.html',{'products':product_lists})
+# getting category
+def category(request):
+    category_lists = Category.objects.all()
+    return render(request,'dashboard_category_lists.html',{'categories':category_lists})
+# update product
+
+def update_product(request, product_id):
+    instance = Product.objects.get(id = product_id)
+    if request.method =='POST':
+        form =ProductForm(request.POST,request.FILES,instance=instance)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Product updated successfully!")
+            return redirect('dashboard_product_lists')
+        else:
+            messages.error(request,"Error,Something went wrong!")
+
+    else:
+        form =ProductForm(instance=instance)
+
+    return render(request,'update_product.html',{'form':form})
+
+# delete product
+
+def delete_product(request, product_id):
+    product=Product.objects.get(id = product_id)
+    product.delete()
+    messages.success(request,'Product deleted successfully!')
+    return redirect('dashboard_product_lists')
+
+# update category
+def update_category(request, category_id):
+    instance= Category.objects.get(id=category_id)
+    if request.method == 'POST':
+        form=CategoryForm(request.POST,request.FILES,instance=instance)
+        if form.is_valid():
+            form.save()
+            messages.success('Category updated successfully!')
+            return redirect('dashboard_category_lists')
+        else:
+            form=CategoryForm(instance=instance)
+    else:
+        return render(request,'update_category.html',{'form':form})
