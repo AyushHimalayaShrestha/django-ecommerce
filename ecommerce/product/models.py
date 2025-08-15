0ebaa7d0c77dcfe75af9222811af351a9ed9e008
Ayush  Shrestha <aayush625@gmail.com>
2025-08-15 13:55:20 +0545

    class cart created

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
+# Category
 class Category(models.Model):
     category_name= models.CharField(max_length=100)
     created_at= models.DateTimeField(auto_now_add=True)
@@ -10,6 +11,7 @@ class Category(models.Model):
     def __str__(self):
      return self.category_name
 
+# Product
 class Product(models.Model):
     product_name= models.CharField(max_length=100, null=True)
     product_price=models.IntegerField(null=True)
@@ -22,4 +24,16 @@ class Product(models.Model):
 
     def __str__(self):
      return self.product_name
+    
+
+# Cart
+
+class Cart(models.Model):
+   user=models.ForeignKey(User, on_delete=models.CASCADE)
+   product= models.ForeignKey(Product,on_delete=models.CASCADE)
+   quantity= models.PositiveIntegerField()
+   created_at= models.DateTimeField(auto_now_add=True)
+   updated_at =models.DateTimeField(auto_now=True)
+
+
 
